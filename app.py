9ed601a6e826d85c5b3efb71353852b17ceff809
Mingheng Wu <wmhst7@gmail.com>
2024-06-03 23:08:55 -0700

    reorg

--- app.py
+++ app.py
@@ -1,76 +1,76 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import plotly.express as px
-from wordcloud import WordCloud, STOPWORDS
-import matplotlib.pyplot as plt
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+import os
+import google.generativeai as genai
+import base64
 
-st.title("Sentiment Analysis of Tweets about US Airlines")
-st.sidebar.title("Sentiment Analysis of Tweets about US Airlines")
+# Load environment variables
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("API_KEY")
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
 
-st.markdown(" This application is a Streamlit app used to analyze the sentiment of the tweets 🐦 about US airlines ✈️ ")
-st.sidebar.markdown(" This application is a Streamlit app used to analyze the sentiment of the tweets 🐦 about US airlines ✈️ ")
+# Initialize chat session if not already done
+if "chat_session" not in st.session_state:
+    st.session_state.chat_session = chat
+    st.session_state.messages = []
 
+# Streamlit page configuration
+st.set_page_config(page_title="PaperStream", page_icon="📄", layout="wide")
 
-DATA_URL = ("./Tweets.csv")
+# Streamlit app main interface
+st.title("PaperStream")
 
-@st.cache_data(persist=True)
-def load_data():
-    data = pd.read_csv(DATA_URL)
-    data['tweet_created'] = pd.to_datetime(data['tweet_created'])
-    # print(type(data))
-    return data
+# Layout configuration
+col1, col2 = st.columns([7, 3])
 
-data = load_data()
+with col1:
+    # Upload PDF and show in viewer
+    uploaded_file = st.file_uploader("Upload your PDF", type=["pdf"])
+    all_text = ""
+    if uploaded_file is not None:
+        # Display PDF using an iframe to show all pages
+        base64_pdf = base64.b64encode(uploaded_file.read()).decode('utf-8')
+        pdf_display = f"<iframe src='data:application/pdf;base64,{base64_pdf}' width='100%' height='100%' style='height:85vh;' type='application/pdf'></iframe>"
+        st.markdown(pdf_display, unsafe_allow_html=True)
 
-st.sidebar.subheader("Show random tweet")
-random_tweet = st.sidebar.radio('Sentiment type', ('positive','negative','neutral'))
-st.sidebar.markdown(data.query('airline_sentiment == @random_tweet')[["text"]].sample(n=1).iat[0,0])
+        # Reset file pointer and extract text
+        uploaded_file.seek(0)
+        reader = PdfReader(uploaded_file)
+        all_text = " ".join([page.extract_text() for page in reader.pages if page.extract_text() is not None])
 
-st.sidebar.markdown("### Number of tweets by sentiment type")
-select = st.sidebar.selectbox('Vizualization type', ['Histogram', 'Pie Chart'], key='1')
-sentiment_count = data['airline_sentiment'].value_counts()
-sentiment_count = pd.DataFrame({'Sentiment' :sentiment_count.index, 'Tweets' :sentiment_count.values})
+with col2:
+    st.header("Interact with Paper")
 
-if not st.sidebar.checkbox('Hide', True):
-    st.markdown("### Number of tweets by Sentiment")
-    if select == "Histogram":
-        fig = px.bar(sentiment_count, x='Sentiment', y='Tweets', color = 'Tweets', height= 500)
-        st.plotly_chart(fig)
-    else:
-        fig = px.pie(sentiment_count, values = 'Tweets', color = 'Sentiment' ,color_discrete_map = {'positive':123,'negative':234,'neutral':345} ,names='Sentiment')
-        st.plotly_chart(fig)
+    # Document summarization
+    st.subheader("Summary of the Paper")
+    with st.spinner('Generating summary ...'):
+        if st.button("Summarize"):
+            summary_query = f"Summarize the following text: {all_text}"
+            summary_response = st.session_state.chat_session.send_message(summary_query)
+            st.write(summary_response.text)
 
-st.sidebar.subheader("When and where are the users tweeting from?")
-hour = st.sidebar.slider("Hour of day", 0, 23)
-modified_data = data[data['tweet_created'].dt.hour == hour]
-if not st.sidebar.checkbox("Close", True, key='2'):
-    st.markdown("### Tweets location based on the time of the day")
-    st.markdown("%i, tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour+1)%24))
-    st.map(modified_data)
-    if st.sidebar.checkbox("Show raw data", False):
-        st.write(modified_data)
+    # Chat interaction
+    st.subheader("Chat with Paper")
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])        
 
+    # Accept user input
+    query = st.chat_input("Ask a question about the paper")
 
-st.sidebar.subheader("Breakdown airline tweets by sentiment")
-choice = st.sidebar.multiselect("Pick airlines", ('US Airways', 'United', 'American', 'Southwest', 'Delta', 'Virgin America'), key = '0')
+    # Calling the Function when Input is Provided
+    if query:
+        with st.chat_message("user"):
+            st.markdown(query)
 
-
-if len(choice) >0:
-    choice_data = data[data.airline.isin(choice)]
-    fig_0 = px.histogram(choice_data, x='airline', y='airline_sentiment', histfunc='count', color='airline_sentiment', facet_col = 'airline_sentiment', labels={'airline_sentiment':'tweets'}, height=600, width=800)
-    st.plotly_chart(fig_0)
-
-st.sidebar.header("Word Cloud")
-word_sentiment = st.sidebar.radio('Display word cloud for which sentiment?', ('positive', 'negative', 'neutral'))
-
-if not st.sidebar.checkbox("Close", True, key = '3'):
-    st.header('Word cloud for %s sentiment' % (word_sentiment))
-    df = data[data['airline_sentiment']==word_sentiment]
-    words = ' '.join(df['text'])
-    processed_words = ' '.join([word for word in words.split() if 'http' not in word and word.startswith('@') and word != 'RT'])
-    wordcloud = WordCloud(stopwords=STOPWORDS, background_color = 'white', height=600, width=800).generate(processed_words)
-    plt.imshow(wordcloud)
-    plt.xticks([])
-    plt.yticks([])
-    st.pyplot()
+        with st.spinner('Generating answer ...'):
+            response = model.generate_content(all_text + query)
+            with st.chat_message("assistant"):
+                st.markdown(response.text)
+            st.session_state.messages.append({"role": "user", "content": query})
+            st.session_state.messages.append({"role": "assistant", "content": response.text})
+            
